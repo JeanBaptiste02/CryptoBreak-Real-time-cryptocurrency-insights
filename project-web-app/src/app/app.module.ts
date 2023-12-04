5ec83c8187e097ddaac16c8c75d1c502e84c8a96
= <damodaranejb@gmail.com>
2023-12-04 22:19:29 +0100

    updated 2 files

--- project-web-app/src/app/app.module.ts
+++ project-web-app/src/app/app.module.ts
@@ -5,16 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { ConnectComponent } from './connect/connect.component';
-import { TestinterfaceComponent } from './testinterface/testinterface.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WelcomePageComponent,
-    ConnectComponent,
-    TestinterfaceComponent,
-  ],
+  declarations: [AppComponent, WelcomePageComponent, ConnectComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
