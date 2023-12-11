9105016d0e3a9934accc8396aaef850180af69f1
= <damodaranejb@gmail.com>
2023-12-11 18:56:22 +0100

    version 1 : implemented service for user login and connected it to the Express backend

--- project-web-app/src/app/connect/connect.component.ts
+++ project-web-app/src/app/connect/connect.component.ts
@@ -1,6 +1,7 @@
+// connect.component.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { ConnectService } from './connect.service';
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.component.html',
@@ -9,9 +10,14 @@ import { Router } from '@angular/router';
 export class ConnectComponent {
   username: string = '';
   password: string = '';
-  constructor(private router: Router) {}
+
+  constructor(private router: Router, private connectService: ConnectService) {}
 
   login(formData: any) {
-    console.log('Login form submitted:', formData);
+    this.connectService
+      .login({ email: this.username, password: this.password })
+      .subscribe((response) => {
+        console.log('Server response:', response);
+      });
   }
 }
---
+++ project-web-app/src/app/connect/connect.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConnectService {
+  private apiUrl = 'http://localhost:4000/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  login(userData: any) {
+    return this.http.post(`${this.apiUrl}/register`, { user: userData });
+  }
+}
