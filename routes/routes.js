fd55fa1a7e2a8111d70b4c13210763a468f4bd57
= <damodaranejb@gmail.com>
2023-12-11 15:08:32 +0100

    removed all files

--- routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const studentController = require("../src/student/studentController");
-
-const router = express.Router();
-
-router.post("/create", studentController.createStudentControllerFn);
-
-module.exports = router;
