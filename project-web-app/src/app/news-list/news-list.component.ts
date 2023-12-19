a8f265db1047589b37534f5d26934a0c75996191
= <damodaranejb@gmail.com>
2023-12-19 17:44:11 +0100

    succeded version1 news/updates page

--- project-web-app/src/app/news-list/news-list.component.ts
+++ project-web-app/src/app/news-list/news-list.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { NewsListService } from '../service/news-list.service';
 
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
-  styleUrl: './news-list.component.css'
+  styleUrl: './news-list.component.css',
 })
 export class NewsListComponent {
+  newsData: any[] = [];
 
+  constructor(private newsListService: NewsListService) {}
+
+  ngOnInit() {
+    this.getNewsData();
+  }
+
+  getNewsData() {
+    this.newsListService.getNewsData('fr', 'fr', 'crypto').subscribe(
+      (data) => {
+        this.newsData = data.results;
+      },
+      (error) => {
+        console.error('Error fetching news data:', error);
+      }
+    );
+  }
 }
