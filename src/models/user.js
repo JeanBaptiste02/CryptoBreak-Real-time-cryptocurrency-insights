2a990200d91c0c7b5ed08471b6eafc9691da305e
Talatizi <talatizikamel88@gmail.com>
2023-12-25 17:06:25 +0100

    update shema users

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,15 @@ const uniqueValidator = require("mongoose-unique-validator");
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  name: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["admin", "utilisateur"],
+    default: "utilisateur",
+  },
+  defaultCurrency: { type: String, default: "EUR" }, // Devise par défaut
+  cryptocurrencies: [{ type: String }], // Liste de crypto-monnaies
+  keywords: [{ type: String }], // Liste de mots-clés
 });
 
 userSchema.plugin(uniqueValidator);
