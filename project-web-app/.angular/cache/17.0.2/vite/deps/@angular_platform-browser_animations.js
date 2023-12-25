5b746c56ebabb4dbed01e061c067ba25a041c222
= <damodaranejb@gmail.com>
2023-12-25 18:52:09 +0100

    updated packages

---
+++ project-web-app/.angular/cache/17.0.2/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,25 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-M33CSEHP.js";
+import "./chunk-KCDD3HND.js";
+import "./chunk-V77MPKEH.js";
+import "./chunk-TUEUM2HG.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-J4GMXRWU.js";
+import "./chunk-2UXUBMH3.js";
+import "./chunk-KUOPV5YS.js";
+import "./chunk-UB6C7KF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
