d69ed710f78174cb31103428c412ee4767bb3e3b
= <damodaranejb@gmail.com>
2024-01-06 15:51:45 +0100

    modified the trendingcontroller to do only 1  request per hour

--- src/controlleur/trendingcoinsController.js
+++ src/controlleur/trendingcoinsController.js
@@ -1,13 +1,26 @@
 const axios = require("axios");
 const Coin = require("../models/trendingCoin");
 
+let lastUpdateTimestamp = 0;
+
 exports.fetchCoinsTrendings = async (req, res) => {
   try {
+    const currentTime = new Date().getTime();
+    const oneHourInMillis = 60 * 60 * 1000;
+
+    if (currentTime - lastUpdateTimestamp < oneHourInMillis) {
+      const storedData = await Coin.find();
+      res.status(200).json(storedData);
+      return;
+    }
+
     const response = await axios.get(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=EUR&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h"
     );
 
-    const trendingdata = response.data.map((coin) => ({
+    await Coin.deleteMany();
+
+    const trendingData = response.data.map((coin) => ({
       name: coin.name,
       image: coin.image,
       current_price: coin.current_price,
@@ -15,21 +28,18 @@ exports.fetchCoinsTrendings = async (req, res) => {
       total_volume: coin.total_volume,
     }));
 
-    await Coin.insertMany(trendingdata);
+    await Coin.insertMany(trendingData);
 
-    res.status(200).json(trendingdata); // Afficher les données
+    lastUpdateTimestamp = currentTime;
+
+    res.status(200).json(trendingData);
   } catch (error) {
-    if (error.response && error.response.status === 429) {
-      const storedData = await Coin.find();
-      res.status(200).json(storedData);
-    } else {
-      console.error(
-        "Erreur lors de la récupération et du stockage des données en tendances :",
-        error
-      );
-      res
-        .status(500)
-        .json({ error: "Erreur interne du serveur", details: error.message });
-    }
+    console.error(
+      "Erreur lors de la récupération et du stockage des données en tendances :",
+      error
+    );
+    res
+      .status(500)
+      .json({ error: "Erreur interne du serveur", details: error.message });
   }
 };
