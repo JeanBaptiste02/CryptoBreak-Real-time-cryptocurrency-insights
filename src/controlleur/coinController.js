db1b7ddc030d2a8b1e68464042994bdb03a6c74b
= <damodaranejb@gmail.com>
2024-01-06 13:06:51 +0100

    controlled number of requests to the api per hour

--- src/controlleur/coinController.js
+++ src/controlleur/coinController.js
@@ -1,8 +1,19 @@
 const axios = require("axios");
 const Coin = require("../models/coin");
 
+let lastUpdateTimestamp = 0;
+
 exports.fetchCoins = async (req, res) => {
   try {
+    const currentTime = new Date().getTime();
+    const oneHourInMillis = 60 * 60 * 1000;
+
+    if (currentTime - lastUpdateTimestamp < oneHourInMillis) {
+      const storedData = await Coin.find();
+      res.status(200).json(storedData);
+      return;
+    }
+
     const response = await axios.get(
       "https://api.coingecko.com/api/v3/coins/markets",
       {
@@ -13,6 +24,8 @@ exports.fetchCoins = async (req, res) => {
         },
       }
     );
+
+    await Coin.deleteMany();
 
     const selectedData = response.data.map((coin) => ({
       id: coin.id,
@@ -38,19 +51,16 @@ exports.fetchCoins = async (req, res) => {
 
     await Coin.insertMany(selectedData);
 
-    res.status(200).json(selectedData); // Afficher les données
+    lastUpdateTimestamp = currentTime;
+
+    res.status(200).json(selectedData);
   } catch (error) {
-    if (error.response && error.response.status === 429) {
-      const storedData = await Coin.find();
-      res.status(200).json(storedData);
-    } else {
-      console.error(
-        "Erreur lors de la récupération et du stockage des données :",
-        error
-      );
-      res
-        .status(500)
-        .json({ error: "Erreur interne du serveur", details: error.message });
-    }
+    console.error(
+      "Erreur lors de la récupération et du stockage des données :",
+      error
+    );
+    res
+      .status(500)
+      .json({ error: "Erreur interne du serveur", details: error.message });
   }
 };
